0eff92d3c059f1a2c4da882aad9d3ad71a00b3e6
nicolasjhampton <nicolasjhampton@gmail.com>
2015-03-27 09:37:08 +0200

    Starting the main game file
    Broke makeMove function down, kept the old makeMove in the main file as a guide.

---
+++ com/mancala/Game.java
@@ -0,0 +1,37 @@
+
+    
+    
+    
+ // a player chooses a cup and makes a move
+    public int makeMove(boolean player, int index) {
+        // get the cup
+        Cup cup = mBoard.getCup(index);
+        // take the beads from the cup
+        int beads = cup.takeBeads();
+        do {
+            // move to the next cup
+            index++;
+            // zero the index to loop around the board
+            if(index >= 14) {
+              index = 0;
+            }
+            // grab the next cup
+            cup = mBoard.getCup(index);
+            // if this is the opponent's home cup
+            if(cup.isHome() == true && player != cup.isFirstPlayer()) {
+            // do nothing and skip the home
+            } else {
+    
+               if(beads == 1 && cup.getBeads() >= 1 && cup.isHome() == false) {
+               // take any beads in the cup if it's the last cup and continue
+               beads += cup.takeBeads();
+               } else {
+               // else drop a bead in the cup
+               beads--;
+               cup.addBeads(1);
+               }
+            }
+        // loop until your out of beads
+        } while (beads > 0);
+       return index;
+    }    
--- com/mancala/models/Mancala.java
+++ com/mancala/models/Mancala.java
@@ -2,53 +2,60 @@ package com.mancala.models;
 
 public class Mancala {
     // True is 1st player, false is second player
-    private boolean mPlayer;
     private CupBoard mBoard;
+    private int mIndex;
+    private int mBeads;
+    private Cup mCup;
     
-    public Mancala(boolean player) {
-        
-        mPlayer = player;
+    public Mancala() {
+        mIndex = 1;
+        mBeads = 0;
+        mCup = new Cup();
         mBoard = new CupBoard();
     }
   
     public CupBoard getBoard() {
       return mBoard;
     }
-  
-  
     
-    // a player chooses a cup and makes a move
-    public void makeMove(boolean player, int index) {
+    public void chooseCup(int index) {
+        // Store the current index
+        mIndex = index;
         // get the cup
-        Cup cup = mBoard.getCup(index);
-        // take the beads from the cup
-        int beads = cup.takeBeads();
-        do {
+        mCup = mBoard.getCup(mIndex);
+        // take the beads from the current cup
+        int mBeads = mCup.takeBeads();
+            
+    }
+    
+    public void getNextCup() {
             // move to the next cup
-            index++;
-            // zero the index to loop around the board
-            if(index >= 14) {
-              index = 0;
+            mIndex++;
+            // loop to the beginning of the board
+            if(mIndex >= 14) {
+              mIndex = 0;
             }
             // grab the next cup
-            cup = mBoard.getCup(index);
-            // if this is the opponent's home cup
-            if(cup.isHome() == true && player != cup.isFirstPlayer()) {
+            mCup = mBoard.getCup(mIndex);
+    }
+    
+    public void settleCup(boolean player) {
+         if(mCup.isHome() == true && player != mCup.isFirstPlayer()) {
             // do nothing and skip the home
             } else {
     
-               if(beads == 1 && cup.getBeads() >= 1 && cup.isHome() == false) {
+               if(mBeads == 1 && mCup.getBeads() >= 1 && mCup.isHome() == false) {
                // take any beads in the cup if it's the last cup and continue
-               beads += cup.takeBeads();
+               mBeads += mCup.takeBeads();
                } else {
                // else drop a bead in the cup
-               beads--;
-               cup.addBeads(1);
+               mBeads--;
+               mCup.addBeads(1);
                }
             }
-        // loop until your out of beads
-        } while (beads > 0);
     }
+        
+    
   
   
 }
